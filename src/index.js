0f1c9cbaf319ca1a89885418a49a4a2c44b03529
Hyesu <hesushim@gmail.com>
2022-08-11 23:14:36 +0900

    노드와 익스프레스로 웹 애플리케이션 만들기

--- src/index.js
+++ src/index.js
@@ -1,2 +1,16 @@
-// index.js
-// This is the main entry point of our application
+// Express.js 모듈 임포트
+const express = require('express');
+// app 객체 생성
+const app = express();
+
+// 애플리케이션 배포시 다른 포트번호 지정할 수 있도록
+const port = process.env.PORT || 4000;
+
+// package.json의 script객체 내의 dev 명령어 nodemon이 index.js파일 변경사항 모니터링
+// npm run dev를 통해 페이지 새로고침시 서버 자동으로 변경
+app.get('/', (req, res) => res.send('Hello Web Server!!!!'));
+
+// 포트번호 지정
+app.listen(port, () =>
+  console.log(`Server running at http://localhost:${port}`)
+);
