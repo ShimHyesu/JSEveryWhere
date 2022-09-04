5c8aea3775da8af9acf13911a71496b3c43875f5
Hyesu <hesushim@gmail.com>
2022-09-04 19:26:16 +0900

    그래프QL API_API 적용

--- src/index.js
+++ src/index.js
@@ -1,31 +1,68 @@
 const express = require('express');
-// apollo-server-express 패키지 포함
 const { ApolloServer, gql } = require('apollo-server-express');
 
-// .env 파일에 명시된 포트 또는 포트 4000에서 서버 실행
 const port = process.env.PORT || 4000;
 
-// 그래프QL 스키마 언어로 스키마를 구성
+// 노트 객체 배열 -> 나중에 데이터베이스로 대체
+let notes = [
+  { id: '1', content: 'This is a note', author: 'Adam Scott' },
+  { id: '2', content: 'This is another note', author: 'Harlow Everly' },
+  { id: '3', content: 'oh hey look, another note!', author: 'Riley Harrison' }
+];
+
+// 스키마: 그래프QL의 데이터 표현 및 상호작용 방식
+// 필수적으로 요구하는 필드 값은 !사용
 const typeDefs = gql`
+  type Note {
+    id: ID!
+    content: String!
+    author: String!
+  }
+
   type Query {
-    hello: String
+    hello: String!
+    notes: [Note!]
+    note(id: ID!): Note!
+  }
+
+  type Mutation {
+    newNote(content: String!): Note!
   }
 `;
 
-// 스키마 필드를 위한 리졸버 함수 제공
-// 사용자에게 값을 반환하는 리졸버 추가
+// 리졸버:API 사용자가 요청한 데이터를 해결
+// API에는 쿼리와 뮤테이션이라는 두가지 자료형의 리졸버가 포함
+// - 쿼리: API에 특정 데이터를 원하는 형식으로 요청
+// - 뮤테이션: API에서 데이터를 수정할때 사용
 const resolvers = {
   Query: {
-    hello: () => 'Hello World!'
+    hello: () => 'Hello World!',
+    notes: () => notes,
+    note: (parent, args) => {
+      return notes.find(note => note.id === args.id);
+    }
+  },
+
+  // 노트 객체를 notes 배열의 메모리에 추가하는 뮤테이션 리졸버 작성
+  Mutation: {
+    newNote: (parent, args) => {
+      let noteValue = {
+        id: String(notes.length + 1),
+        content: args.content,
+        author: 'Adam Scott'
+      };
+
+      notes.push(noteValue);
+      // 객체를 반환하면 그래프QL 뮤테이션이 원하는 형식으로 응답 받을 수 있음
+      return noteValue;
+    }
   }
 };
 
 const app = express();
 
-// 아폴로 서버 설정
 const server = new ApolloServer({ typeDefs, resolvers });
 
-// 아폴로 그래프QL 미들웨어 적용하고 경로를 /api로 설정
 server.applyMiddleware({ app, path: '/api' });
 
 app.listen({ port }, () =>
